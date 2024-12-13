27a81a879c177961907fd0d4f1befbf4145be211
Juliana Manzieri <julianaluckmann@hotmail.com>
2024-12-13 10:29:55 +0100

    first commit

---
+++ src/test/java/com/julianamanzieri/forum_hub/ForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianamanzieri.forum_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
